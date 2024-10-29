64e24d5d6f0d695aa1758b515e4973168824b10f
vekim00100 <vekim@ucsc.edu>
2024-10-29 12:05:53 -0700

    Add a pumpkin button that you can click

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Adding button function
+const button = document.createElement("button");
+button.textContent = "🎃";
+app.append(button);
