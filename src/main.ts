b2162cb94b78e978bd29450d72c4b434c1dc4181
vekim00100 <vekim@ucsc.edu>
2024-10-31 01:44:56 -0700

    Step 2: Clicking increases a counter

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,20 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// Initialize click counter
+let counter: number = 0;
+
+// Display counter
+const counterDiv = document.createElement("div");
+counterDiv.textContent = `${counter} Pumpkins`;
+app.append(counterDiv);
+
 // Adding button function
 const button = document.createElement("button");
 button.textContent = "🎃";
 app.append(button);
+
+button.addEventListener('click', () => {
+    counter++;
+    counterDiv.textContent = `${counter} Pumpkins`;
+});
